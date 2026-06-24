dec07f28cc4ed1e878d25999491b05df36163b10
Anahjoe <support@tradebaba.ng>
2026-06-24 19:40:48 +0100

    Fix gitignore location, untrack node_modules and dist

--- dist/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-export const adminMiddleware = (req, res, next) => {
-    const adminKey = req.headers['x-admin-key'];
-    if (!adminKey || adminKey !== process.env.ADMIN_KEY) {
-        return res.status(403).json({ error: 'Admin access required' });
-    }
-    next();
-};
-//# sourceMappingURL=admin.js.map
